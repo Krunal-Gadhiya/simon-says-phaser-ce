6b57a1cefb79e7fd67440a08c25fb8bfec90d98e
Krunal Gadhiya <krunalgadhiya3@gmail.com>
2022-02-15 19:10:11 +0530

    -add sequence recording and matching

--- states/gameplay.js
+++ states/gameplay.js
@@ -1,6 +1,8 @@
 
 export default class GameplayState extends Phaser.State {
     init() {
+        this.round = 1;
+        this.canClick = false;
 
     }
 
@@ -27,17 +29,60 @@ export default class GameplayState extends Phaser.State {
 
         this.blocks = [this.blue, this.yellow, this.green, this.red];
 
+        this.submit = this.game.add.text(450, 100, 'Sumbit', {
+            font: 'Arail',
+            fontSize: 50,
+            fill: '#ffffff',
+            align: 'center'
+        });
+        this.submit.anchor.set(0.5, 0.5);
+        this.submit.inputEnabled = true;
+        this.submit.events.onInputDown.add(this.onSubmit, this);
+
         this.addInput();
 
-        this.round = 1;
         this.game.time.events.add(1000, this.showPattern, this);
+    }
+
+    onSubmit() {
+        console.log('submit tapped');
+        console.log('crnt - ', this.crntSequence);
+        console.log('recorded - ', this.recordSequence);
+
+        let isAnswerCorrect = true;
+        if (this.recordSequence.length !== this.crntSequence.length) {
+            isAnswerCorrect = false;
+        }
+
+        if (isAnswerCorrect) {
+            for (let i = 0; i < this.recordSequence.length; i++) {
+                if (this.recordSequence[i] !== this.crntSequence[i]) {
+                    isAnswerCorrect = false;
+                    break;
+                }
+            }
+        }
+
+        if (!isAnswerCorrect) {
+            console.log('failed');
+        }
+        else {
+            console.log('correct match');
+        }
     }
 
     showPattern() {
         this.crntSequence = [];
+        this.recordSequence = [];
         for (let i = 0; i < this.round; i++) {
-            this.crntSequence.push(this.findColor());
+            const color = this.findColor();
+            this.crntSequence.push(color.colorID);
+            this.bounceTheBlock(color);
         }
+
+        this.game.time.events.add(1000, () => {
+            this.canClick = true;
+        });
 
         this.crntSequence.forEach(block => {
             console.log(block.colorID);
@@ -56,7 +101,17 @@ export default class GameplayState extends Phaser.State {
     }
 
     onBlockTap(block, pointer) {
-        console.log('block tapped');
+        if (!this.canClick) return;
+
+        console.log('block tapped', block.colorID);
+        this.recordSequence.push(block.colorID);
+    }
+
+    bounceTheBlock(block) {
+        this.game.add.tween(block.scale).to({
+            x: 1.2,
+            y: 1.2
+        }, 350, Phaser.Easing.Back.InOut, true, 0, 0, true);
     }
 
     update() {
